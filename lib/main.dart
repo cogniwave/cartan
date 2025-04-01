01ac4197f71d01d8d7d1f1e31f306cddb931166a
valkopt00 <valkopt@gmail.com>
2025-04-01 18:16:24 +0100

    Implement Bugsnag: add error monitoring and crash reporting

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,44 @@ import 'package:cartan/utils/flavor_config.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:cartan/src/themes/app_themes.dart';
+import 'package:bugsnag_flutter/bugsnag_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:async';
 
-void initializeApp() {
+Future<void> initializeApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
+  // final bugsnagApiKey = 'd591b3ade83580c510f9fc556bfe7115';
+
+  final bugsnagApiKey = dotenv.env['TESTE_API_KEY'] ?? 'BUGSNAG_API_KEY';
+
+  
+  await bugsnag.start(
+    apiKey: bugsnagApiKey,
+    releaseStage: FlavorConfig.instance.flavor.toString().split('.').last,
+    enabledReleaseStages: {'dev', 'staging', 'prod'},
+    appVersion: '1.0.0',
+    metadata: {
+      'app': {
+        'environment': FlavorConfig.instance.flavor.toString().split('.').last,
+        'apiBaseUrl': FlavorConfig.instance.values.apiBaseUrl,
+        'appName': FlavorConfig.instance.values.appName,
+      }
+    },
+  );
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    bugsnag.notify(details.exception, details.stack);
+    FlutterError.presentError(details);
+  };
+
+  runZonedGuarded(() {
+    runApp(const MyApp());
+  }, (Object error, StackTrace stack) {
+    bugsnag.notify(error, stack);
+  });
+  
   runApp(const MyApp());
 }
 
@@ -18,7 +54,7 @@ class MyApp extends StatelessWidget {
       title: FlavorConfig.instance.values.appName,
       theme: AppThemes.darkTheme,
       supportedLocales: const [
-        Locale('pt', ''), // Português
+        Locale('pt', ''),
       ],
       localizationsDelegates: const [
         AppLocalizations.delegate,
@@ -26,7 +62,10 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      home: const HomePage(), // Não passes o título aqui
+      navigatorObservers: [
+        BugsnagNavigatorObserver(),
+      ],
+      home: const HomePage(),
     );
   }
 }
