0c9772e2990c5b8d00148c141fa4e7387d905751
valkopt00 <valkopt@gmail.com>
2025-05-28 12:35:52 +0100

    [chore] localization + versionCode + gradle tweaks

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             ndk {
                 debugSymbolLevel = "FULL"
             }
